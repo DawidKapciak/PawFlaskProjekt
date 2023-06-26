65a536a9fb4fe28cb6ff739f0a5f154defe8d433
DawidKapciak <dawidkapciak@gmail.com>
2023-06-26 21:53:41 +0200

    v0.4 Adding: - swagger base, - REST api for adding, modifying and deleting notes, Modifying: - note model, - db initialization file,

--- app.py
+++ app.py
@@ -11,7 +11,26 @@ from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField, SubmitField, PasswordField, EmailField
 from wtforms.validators import DataRequired, Email, Length
 from werkzeug.utils import secure_filename
+from flask_swagger_ui import get_swaggerui_blueprint
 
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/swagger.json'  # Our API url (can of course be a local resource)
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
 
 load_dotenv()
 
@@ -33,7 +52,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
 app.secret_key = os.getenv("SECRET_KEY")
 app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024
-
+app.register_blueprint(swaggerui_blueprint)
 db = SQLAlchemy(app)
 
 
@@ -64,20 +83,15 @@ class ProfilePicForm(FlaskForm):
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
+    user = db.Column(db.String(200), nullable=False)
     title = db.Column(db.String(50), nullable=False)
     text = db.Column(db.String(200), nullable=False)
-    category_id = db.Column(db.Integer)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
-    date_updated = db.Column(db.DateTime)
-    date_ending = db.Column(db.DateTime)
-    sharing_id = db.Column(db.Integer, nullable=True)
 
 
 class NoteForm(FlaskForm):
     title = StringField("Tytuł notatki", validators=[DataRequired()])
     text = StringField("Treść notatki", validators=[DataRequired()])
-    category = StringField("Kategoria")
     save = SubmitField("Zapisz")
 
 
@@ -86,7 +100,7 @@ def add_note():
     form = NoteForm()
     our_notes = Note.query.order_by(Note.date_added)
     if form.validate_on_submit():
-        note = Note(user_id=2, title=form.title.data, text=form.text.data, category_id=0)
+        note = Note(user_id=2, title=form.title.data, text=form.text.data)
         try:
             db.session.add(note)
             db.session.commit()
@@ -101,15 +115,24 @@ def add_note():
                            form=form)
 
 
+@app.route('/notes/add', methods=['POST'])
+def add_note_rest():
+    note = Note(title=request.json['title'], text=request.json['text'])
+
+    db.session.add(note)
+    db.session.commit()
+
+    return {'id': note.id, 'title': note.title, 'text': note.text}
+
+
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit_note(id):
     form = NoteForm()
-    our_notes = Note.query.order_by(Note.date_added)
+    our_notes = Note.query.filter_by(user=session['user'])
     note_to_update = Note.query.get_or_404(id)
     if request.method == "POST":
         note_to_update.title = request.form['title']
         note_to_update.text = request.form['text']
-        note_to_update.category = request.form['category']
         try:
             db.session.commit()
             flash("Zmodyfikowano notatkę.")
@@ -122,6 +145,19 @@ def edit_note(id):
         return render_template("edit_note.html",
                                form=form,
                                note_to_update=note_to_update)
+
+
+@app.route('/notes/edit/<int:id>', methods=['PUT'])
+def edit_note_rest(id):
+    note_to_update = Note.query.get(id)
+    if note_to_update is None:
+        return {'error': 'note not found'}
+    else:
+        note_to_update.title = request.json['title']
+        note_to_update.text = request.json['text']
+        db.session.commit()
+
+    return {'id': note_to_update.id, 'title': note_to_update.title, 'text': note_to_update.text}
 
 
 @app.route('/delete/<int:id>', methods=['GET', 'POST'])
@@ -140,22 +176,28 @@ def delete_note(id):
                                our_notes=our_notes)
 
 
+@app.route('/notes/delete/<int:id>', methods=['DELETE'])
+def delete_note_rest(id):
+    note_to_delete = Note.query.get(id)
+    if note_to_delete is None:
+        return {'error': 'note not found'}
+    db.session.delete(note_to_delete)
+    db.session.commit()
+    return {'message': 'note deleted!'}
+
+
 def create_name(email):
     index = email.find('@')
     name = email
-    # Sprawdź, czy znaleziono znak '@'
     if index != -1:
-        # Usuń wszystko, co jest po znaku '@'
         name = email[:index]
     return name
 
 
-# Create a route decorator
 @app.route('/', methods=['POST', 'GET'])
 def login():
     email = None
     password = None
-    our_notes = Note.query.order_by(Note.date_added)
     form = LoginForm()
     if form.validate_on_submit():
         email = form.email.data
@@ -167,7 +209,6 @@ def login():
                 session['user'] = email
                 session['name'] = create_name(email)
                 session['idToken'] = user['idToken']
-                print(storage.child(f"images/profile_pic.png").get_url(None))
                 img_url = storage.child(f"images/profile_pic_{session['user']}.jpg").get_url(None)
                 response = requests.get(img_url)
                 if response.status_code == 200:
@@ -184,6 +225,7 @@ def login():
                 flash("Podałeś błędne hasło lub takie konto z takim adresem email nie istnieje.")
 
     if 'user' in session:
+        our_notes = Note.query.filter_by(user=session['user'])
         return render_template('index.html', our_notes=our_notes)
 
     return render_template('login.html', email=email, password=password, form=form)
@@ -275,22 +317,20 @@ def get_notes():
     output = []
     for note in notes:
         note_data = {
-            'id': note.id, 'user_id': note.user_id, 'title': note.title,
-            'text': note.text, 'category_id': note.category_id, 'date_added': note.date_added,
-            'date_updated': note.date_updated, 'date_ending': note.date_ending, 'sharing_id': note.sharing_id
+            'id': note.id, 'user': note.user, 'title': note.title,
+            'text': note.text, 'date_added': note.date_added
         }
         output.append(note_data)
     return {'notes': output}
 
 
-@app.route('/note/<id>')
+@app.route('/notes/<id>')
 def get_note(id):
     note = Note.query.get_or_404(id)
 
     return {
-        'id': note.id, 'user_id': note.user_id, 'title': note.title,
-        'text': note.text, 'category_id': note.category_id, 'date_added': note.date_added,
-        'date_updated': note.date_updated, 'date_ending': note.date_ending, 'sharing_id': note.sharing_id
+        'id': note.id, 'user': note.user, 'title': note.title,
+        'text': note.text, 'date_added': note.date_added
     }
 
 
--- db_init.py
+++ db_init.py
@@ -7,24 +7,20 @@ cursor = conn.cursor()
 # Utwórz tabelę 'Note' z wymaganymi kolumnami, jeśli nie istnieje
 cursor.execute('''CREATE TABLE IF NOT EXISTS note
                   (id INTEGER PRIMARY KEY,
-                   user_id INTEGER,
+                   user TEXT,
                    title TEXT,
                    text TEXT,
-                   category_id INTEGER,
-                   date_added datetime,
-                   date_updated datetime,
-                   date_ending datetime,
-                   sharing_id INTEGER)''')
+                   date_added datetime)''')
 print("Stworzono tabelę 'note'")
 # Przykładowe dane
 data = [
-    (1, 1, 'Tytuł notatki 1', 'Treść notatki 1', 1, '2023-01-01', '2023-01-02', '2023-01-05', 1),
-    (2, 1, 'Tytuł notatki 2', 'Treść notatki 2', 2, '2023-02-01', '2023-02-02', '2023-02-05', 0),
-    (3, 2, 'Tytuł notatki 3', 'Treść notatki 3', 1, '2023-03-01', '2023-03-02', '2023-03-05', 1)
+    (1, "dawidkapciak@gmail.com", 'Tytuł notatki 1', 'Treść notatki 1', '2023-01-01'),
+    (2, "FlaskPaw@gmail.com", 'Tytuł notatki 2', 'Treść notatki 2', '2023-02-01'),
+    (3, "FlaskPaw@gmail.com", 'Tytuł notatki 3', 'Treść notatki 3', '2023-03-01')
 ]
 
 # Wstaw dane do tabeli 'Note'
-cursor.executemany('INSERT INTO note VALUES (?,?,?,?,?,?,?,?,?)', data)
+cursor.executemany('INSERT INTO note VALUES (?,?,?,?,?)', data)
 
 # Zatwierdź zmiany
 conn.commit()
